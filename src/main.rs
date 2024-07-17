85d8b6ad77b382cacfbf19c0cc806f5fd29ca03d
Lin Jiang <lnjng07@protonmail.com>
2024-07-16 20:07:11 -0400

    add verbose option

--- src/main.rs
+++ src/main.rs
@@ -31,28 +31,43 @@ EXAMPLES:
 
 /// pretty prints number in all bases
 fn print_num(num: i64) {
-    println!("decimal: {}", num);
-    println!("hexadecimal: 0x{:x}", num);
-    println!("octal: 0o{:o}", num);
-    println!("binary: 0b{:b}", num);
+    print!("\x1B[1mdec:\x1B[0m {} \t", num);
+    print!("\x1B[1mhex:\x1B[0m 0x{:x} \t", num);
+    print!("\x1B[1moct:\x1B[0m 0o{:o} \t", num);
+    print!("\x1B[1mbin:\x1B[0m 0b{:b} \t", num);
 }
 
 fn main() {
+    // handle command line arguments
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 { error("invalid number of arguments"); }
+    let verbose = args.len() > 2 && args[2] == "--verbose";
+    if args.len() < 2 { error("invalid number of arguments"); }
     if args[1] == "--help" { usage(); process::exit(0); }
+
     let input = args[1].replace(" ", "");
     let tokens: Result<Vec<Token>, &str> = parse(&input);
     if tokens.is_err() { error(tokens.as_ref().err().unwrap()); }
-    /*
-    for token in tokens.clone().unwrap().iter() {
-        println!("normal: {:?}", token);
+
+    // print infix tokens if verbose
+    if verbose {
+        println!("--- infix tokens ---");
+        for token in tokens.clone().unwrap().iter() {
+            println!("{:?}", token);
+        }
+        println!();
     }
-    */
+
     let shunted: Vec<Token> = shunt(tokens.unwrap());
-    for token in shunted.iter() {
-        println!("{:?}", token);
+
+    // print postfix tokens if verbose
+    if verbose {
+        println!("--- postfix tokens ---");
+        for token in shunted.iter() {
+            println!("{:?}", token);
+        }
+        println!();
     }
+
     let result = interpret(shunted);
     print_num(result);
 }
